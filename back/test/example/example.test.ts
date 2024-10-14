480d3a3d82f378ba8b886e39584dac65b3886e61
cincarnato <cristian.incarnato@sondeos.com.ar>
2024-10-14 12:03:14 -0300

    chore: add test example

---
+++ back/test/example/example.test.ts
@@ -0,0 +1,12 @@
+import {describe, test} from "node:test"
+import * as assert from "assert";
+
+describe("Base Test", () => {
+
+
+    test('Simple test', async () => {
+
+        assert.equal(1, 1)
+    })
+
+})
