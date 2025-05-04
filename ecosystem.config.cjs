c9424d6b2e9c9bbe32ad2acf8d69ab4c5b36b23f
cincarnato <cristian.incarnato@sondeos.com.ar>
2025-05-04 09:12:34 -0300

    chore: update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,14 +10,21 @@ module.exports = {
 				PORT: '9090',
 
 				//JWT
-				DRAX_JWT_SECRET:'u2OcSvAOk7e8Jbknpm33nwgf',
-				DRAX_JWT_EXPIRATION:'2h',
-				DRAX_JWT_ISSUER:'DRAX',
+				DRAX_JWT_SECRET: '',
+				DRAX_JWT_EXPIRATION: '2h',
+				DRAX_JWT_ISSUER: 'DRAX SCAFFOLD',
+				DRAX_APIKEY_SECRET:'',
 
 				//DB
-				DRAX_DB_ENGINE:'mongo', //sqlite or mongo
-				DRAX_MONGO_URI:'mongodb://127.0.0.1:27017/drax',
-				DRAX_SQLITE_FILE:'drax.db',
+				DRAX_DB_ENGINE: 'mongo', //sqlite or mongo
+				DRAX_MONGO_URI: 'mongodb://127.0.0.1:27017/drax-scaffold',
+				DRAX_SQLITE_FILE: 'drax.db',
+
+				//#MEDIA
+				DRAX_PORT: '8080',
+				DRAX_BASE_URL: 'http://localhost:9090',
+				DRAX_MAX_UPLOAD_SIZE: '5000000',
+				DRAX_FILE_DIR: 'uploads',
 			}
 		}
 
