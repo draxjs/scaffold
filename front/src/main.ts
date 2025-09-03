3bf03295683ae602a0868af15bec506bd7d94b26
cincarnato <cristian.incarnato@sondeos.com.ar>
2025-09-03 19:17:25 -0300

    feat: setup pinia and setting first

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,31 +1,28 @@
-// Plugins
-// @ts-ignore
-import { registerPlugins } from '@/plugins'
 
 
 import i18n from "@/i18n/I18n";
 
-// Components
+/// Components
 import App from './App.vue'
+import vuetify from "@/plugins/vuetify";
+import router from "@/router";
+import pinia from "@/stores";
 
 // Composables
 import { createApp } from 'vue'
-import {setupAuth} from "@/setup/SetupAuth";
-import {useSetting} from "@drax/settings-vue";
-
-
+import {setupAuth} from "./setup/SetupAuth";
 
 const app = createApp(App)
-registerPlugins(app)
 
-app.use(i18n)
+app.use(pinia)
 
-//Load Settings
+import { useSetting } from '@drax/settings-vue'
 const {fetchSettings, suscribeAuth} = useSetting()
 await fetchSettings()
 await suscribeAuth()
 
-//Core Systems Factories
+app.use(vuetify).use(router).use(i18n)
+
 setupAuth()
 
 
