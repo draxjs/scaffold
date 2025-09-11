80da7dffa004b1e4db47efb6dc15ca55624d4a4b
cincarnato <cristian.incarnato@sondeos.com.ar>
2025-09-11 16:59:17 -0300

    misc: update drax versions improve: setup frontend main settings

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,30 +1,39 @@
-
-
-import i18n from "@/i18n/I18n";
-
-/// Components
 import App from './App.vue'
+import i18n from "@/i18n/I18n";
 import vuetify from "@/plugins/vuetify";
 import router from "@/router";
 import pinia from "@/stores";
-
-// Composables
 import { createApp } from 'vue'
 import {setupAuth} from "./setup/SetupAuth";
 
+//Setup App
 const app = createApp(App)
 
+//Setup Pinia
 app.use(pinia)
 
+//Setup Settings
 import { useSetting } from '@drax/settings-vue'
 const {fetchSettings, suscribeAuth} = useSetting()
 await fetchSettings()
 await suscribeAuth()
 
-app.use(vuetify).use(router).use(i18n)
+//Setup Custom Identity Cruds
+// import { useIdentityCrudStore } from '@drax/identity-vue'
+// import CustomUserCrud from './modules/base/cruds/CustomUserCrud'
+// import CustomRoleCrud from "./modules/base/cruds/CustomRoleCrud";
+// const identityCrudStore = useIdentityCrudStore()
+// identityCrudStore.setUserCrud(new CustomUserCrud())
+// identityCrudStore.setRoleCrud(new CustomRoleCrud())
 
+//Setup Router, I18n and Vuetify
+app
+  .use(vuetify)
+  .use(router)
+  .use(i18n)
+
+//Setup Auth
 setupAuth()
 
-
-
+//Mount App
 app.mount('#app')
