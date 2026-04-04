f34b2d02c1aafe9117a50d83be493d9a63f9448a
cincarnato <cristian.incarnato@sondeos.com.ar>
2026-04-04 10:11:38 -0300

    chore: build local

--- deploy_local.sh
+++ deploy_local.sh
@@ -6,8 +6,8 @@ git checkout .
 git pull
 
 
-echo "DEPLOY: Building"
-sh build.sh
+echo "DEPLOY: building"
+sh build_local.sh
 
 
 echo "DEPLOY: Install production dependencies"
@@ -18,3 +18,4 @@ cd ..
 
 echo "DEPLOY: START SERVICE"
 sh start.sh
+
