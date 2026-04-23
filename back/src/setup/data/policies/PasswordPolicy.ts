5b77c6de7e5449927adba274525a3694fdfa7665
cincarnato <cristian.incarnato@sondeos.com.ar>
2026-04-23 11:59:32 -0300

    chore: upgrade drax

--- back/src/setup/data/policies/PasswordPolicy.ts
+++ back/src/setup/data/policies/PasswordPolicy.ts
@@ -3,10 +3,10 @@ import type {IPasswordPolicy} from "@drax/identity-share";
 const projectPasswordPolicy: IPasswordPolicy = {
     minLength: 8,
     maxLength: 32,
-    requireUppercase: false,
-    requireLowercase: false,
-    requireNumber: false,
-    requireSpecialChar: false,
+    requireUppercase: true,
+    requireLowercase: true,
+    requireNumber: true,
+    requireSpecialChar: true,
     disallowSpaces: false,
     preventReuse: 3,
     expirationDays: null
